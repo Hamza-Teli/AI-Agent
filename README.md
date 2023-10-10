c95945961334c340f9d490769d2fcdc4aaae989c
Hamza Teli <hteli1@myseneca.ca>
2023-10-10 09:44:04 -0400

    Setup all OpenAI and langchain

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # AI-Agent
 Experimenting how to code an AI Agent using Langchain
 
+## How to Setup AI agent
+1) Install dot env: pip install python-dotenv
+2) Install openai: pip install openai
+3) Install langchain: pip install langchain
 
 Resources used: https://www.youtube.com/watch?v=2TQ43quMKbE
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
 
@@ -14,3 +16,23 @@ dotenv_path = os.path.join(parent_dir, '.env')
 # Load the .env file from parent directory
 load_dotenv(dotenv_path)
 
+
+# ALL GPT BELOW ----------------------------------------------------
+llm = OpenAI(temperature=0)
+
+# Below is GPT 3.5 turbo
+chat_llm = ChatOpenAI(temperature=0)
+print(chat_llm.model_name)
+
+# If you want to use a specific model use the code below!
+# modelYouWant = ChatOpenAI(model="gpt-4", temperature=0)
+
+# Below is the langchain model
+print(llm.model_name)
+get_user_input = input("[LANGCHAIN] Enter a prompt: ")
+prediction = llm.predict(get_user_input)
+print(prediction)
+
+# ChatGPT 3.5 Model
+get_user_input_for_gpt3 = input("[GPT3.5 Turbo] Enter a prompt: ")
+print(chat_llm.predict(get_user_input_for_gpt3))
